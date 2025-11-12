d7a1582860ba8345e33634061f1e3eef51cdd4dd
MrFranxis <franciscodnag@gmail.com>
2025-11-12 22:47:27 +0100

    actualizacion conexion con base de datos

---
+++ database/data12112025.sql
@@ -0,0 +1,213 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: b_day_connect
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'familia'),(2,'amigos'),(3,'pareja'),(4,'trabajo'),(5,'conocidos');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contacto_categoria`
+--
+
+DROP TABLE IF EXISTS `contacto_categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacto_categoria` (
+  `id_contacto` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id_contacto`,`id_categoria`),
+  KEY `id_categoria` (`id_categoria`),
+  CONSTRAINT `contacto_categoria_ibfk_1` FOREIGN KEY (`id_contacto`) REFERENCES `contactos` (`id_contacto`),
+  CONSTRAINT `contacto_categoria_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacto_categoria`
+--
+
+LOCK TABLES `contacto_categoria` WRITE;
+/*!40000 ALTER TABLE `contacto_categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacto_categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contacto_gustos`
+--
+
+DROP TABLE IF EXISTS `contacto_gustos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacto_gustos` (
+  `id_contacto` int(11) NOT NULL,
+  `id_gustos` int(11) NOT NULL,
+  PRIMARY KEY (`id_contacto`,`id_gustos`),
+  KEY `id_gustos` (`id_gustos`),
+  CONSTRAINT `contacto_gustos_ibfk_1` FOREIGN KEY (`id_contacto`) REFERENCES `contactos` (`id_contacto`),
+  CONSTRAINT `contacto_gustos_ibfk_2` FOREIGN KEY (`id_gustos`) REFERENCES `gustos` (`id_gustos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacto_gustos`
+--
+
+LOCK TABLES `contacto_gustos` WRITE;
+/*!40000 ALTER TABLE `contacto_gustos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contacto_gustos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contactos`
+--
+
+DROP TABLE IF EXISTS `contactos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contactos` (
+  `id_contacto` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `fecha_registro` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_contacto`),
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `contactos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactos`
+--
+
+LOCK TABLES `contactos` WRITE;
+/*!40000 ALTER TABLE `contactos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contactos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gustos`
+--
+
+DROP TABLE IF EXISTS `gustos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gustos` (
+  `id_gustos` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_gusto` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_gustos`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gustos`
+--
+
+LOCK TABLES `gustos` WRITE;
+/*!40000 ALTER TABLE `gustos` DISABLE KEYS */;
+INSERT INTO `gustos` VALUES (1,'cine'),(2,'videojuegos'),(3,'arte'),(4,'lectura'),(5,'musica'),(6,'viajar'),(7,'cine'),(8,'videojuegos'),(9,'arte'),(10,'lectura'),(11,'musica'),(12,'viajar');
+/*!40000 ALTER TABLE `gustos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'usuario');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `fecha_registro` datetime DEFAULT current_timestamp(),
+  `id_rol` int(11) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id_rol` (`id_rol`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Juan Cayetano','adminprincipal@tfg.com','$2y$10$dn.gxq0Pvwe5DORaFqQUyeWzyXcY1axH0XjNlgYN1qdzT3dlS37Vu','1989-12-31','2025-11-06 18:40:50',1),(2,'Juan Jose','juanjoproyecto@tfg','$2y$10$M4BmmZU5EXvipJbh9hy7CuNOzm/byvO5h1pf3Wn/eB/jk8A/wk1EW','1989-12-24','2025-11-07 19:49:00',2),(3,'Juan Perez','juanpe@tfg.com','$2y$10$7mZ1.iw19lUhSHh0JSVX4OAsJMmjVNYnXDn0Olu57MJvV4otxCi3i','1990-03-01','2025-11-07 19:52:19',2),(5,'Lucia Ruiz','luciaproyecto@tfg.com','$2y$10$xx5b3h0vDkN6PaBQF9yAluiDj5dnA2f5qHZhTseWVTbD3njtI/23e','1996-07-29','2025-11-08 21:41:56',2),(6,'Paco Paco','pacoproyecto@tfg.com','$2y$10$bMZ5PiGnVP6DSBmRlo2rI.rTc/PC/ZiXuY7bU6ZYuaEJ1e3HZ961u','1990-01-01','2025-11-10 11:15:20',2);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-12 14:08:45
