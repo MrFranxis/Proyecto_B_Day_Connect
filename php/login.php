54d53b10a570bdc8c8f26a0b9b7cbe5686dff3d7
MrFranxis <franciscodnag@gmail.com>
2025-11-15 14:46:35 +0100

    correccion

--- php/login.php
+++ php/login.php
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-<?php
-//login.php
 
-session_start();
-require_once("conexion.php");
-
-if (!$conn) {
-    die("Error: Conexión a la base de datos fallida.");
-}
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = trim($_POST["email"]);
-    $password = trim($_POST["password"]);
-
-    try {
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
-        $stmt->bindParam(":email", $email);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if($password == $user["password"]) {
-                $_SESSION["id_usuario"] = $user["id_usuario"];
-                $_SESSION["nombre"] = $user["nombre"];
-                $_SESSION["rol"] = $user["id_rol"];
-
-                if ($user["id_rol"] == 1) {
-                    header("Location: ../admin/home_admin.php");
-                } else {
-                    header("Location: ../user/home_user.php");
-                }
-                exit;
-            } else {
-                echo "<script>alert('Contraseña incorrecta'); window.history.back();</script>";
-            }
-        } else {
-            echo "<script>alert('Usuario no encontrado'); window.history.back();</script>";
-        }
-    } catch (PDOException $e) {
-        echo "Error en la consulta: " . $e->getMessage();
-    }
-}
-=======
 <?php
 
 session_start();
@@ -82,5 +38,5 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
 }
 
 
->>>>>>> c9a44ca (Se unifican  ramas, se actualiza dump)
+
 ?>
